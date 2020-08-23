acd842433e0a849f58a366c21e98db3211def9ed
HP <HP@vineshGuggilla>
2020-08-23 16:27:44 +0530

    comitted

--- helloworld/src/main/java/com/helloworld/IndexController.java
+++ helloworld/src/main/java/com/helloworld/IndexController.java
@@ -63,7 +63,7 @@ public class IndexController {//com.helloworld.IndexController
 		System.out.println("email : "+email);
 		m.addAttribute("user",loguser);
 		System.out.println("exit from logUser method");
-
+   System.out.println("vinesh");
         
 
 		System.out.println("master commits 2nd time");
